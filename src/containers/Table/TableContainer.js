65bfa5a7d703d67efd1bb3eed23e094b55c8668e
oscaroceguera <oscaroceguera@gmail.com>
2017-04-11 03:44:02 -0500

    [Container] ::: Wrapper, Header  & Table>TableHeader
    Static UI

--- src/containers/Table/TableContainer.js
+++ src/containers/Table/TableContainer.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router'
 
+import Wrapper from '../../components/commons/Wrapper'
+import Header from '../../components/Header/Header'
+import Table from '../../components/Table/Table'
+
 class TableContainer extends Component {
   render () {
     return (
       <div>
-        <h1>{'Table Container'}</h1>
         {/* Back to home */}
         <Link to='/'>{'Home'}</Link>
+
+        <Wrapper>
+          <Header />
+          <Table />
+        </Wrapper>
       </div>
     )
   }
