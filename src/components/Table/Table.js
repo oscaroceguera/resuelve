e42e179f30047b80f8ae6201c9a588cf01ab6250
oscaroceguera <oscaroceguera@gmail.com>
2017-04-11 13:15:52 -0500

    [Component] ::: Static conceptos data & import Row & Footer components

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,15 +1,30 @@
 import React from 'react'
 
 import TableHeader from './TableHeader'
+import TableRow from './TableRow'
+import TableFooter from './TableFooter'
 
 const styles = {
   width: '95%',
   margin: '0 auto'
 }
 
+const data = [
+  {descripcion: 'Control Xbox', cantidad: 1, unidades: 12, precio: '999.00', total: '999.00'},
+  {descripcion: 'Control Xbox', cantidad: 1, unidades: 12, precio: '999.00', total: '999.00'},
+  {descripcion: 'Control Xbox', cantidad: 1, unidades: 12, precio: '999.00', total: '999.00'},
+  {descripcion: 'Control Xbox', cantidad: 1, unidades: 12, precio: '999.00', total: '999.00'},
+  {descripcion: 'Control Xbox', cantidad: 1, unidades: 12, precio: '999.00', total: '999.00'},
+  {descripcion: 'Control Xbox', cantidad: 1, unidades: 12, precio: '999.00', total: '999.00'}
+]
+
 const Table = () => (
   <div style={styles}>
     <TableHeader />
+    {
+      data.map((item, key) => <TableRow key={key} item={item} />)
+    }
+    <TableFooter />
   </div>
 )
 
