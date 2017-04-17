ed674b1f16740647bdba1edea8ef36ce81344059
oscaroceguera <oscaroceguera@gmail.com>
2017-04-17 01:12:05 -0500

    [Component] ::: Update Option and decimal function

--- src/components/Table/TableRow.js
+++ src/components/Table/TableRow.js
@@ -3,18 +3,23 @@ import UpdateIcon from '../../svg/update.svg'
 import DeleteIcon from '../../svg/delete.svg'
 import './styles.css'
 
-const TableRow = ({item, deleteConcepto, index}) => (
+function decimal(value) {
+  value = parseFloat(value)
+  return value.toFixed(2)
+}
+
+const TableRow = ({item, deleteConcepto, updateConcepto, index}) => (
     <div className='tableRow'>
       <div className='tableRow__descripcion xl'>{item.descripcion}</div>
       <div className='tableRow__cantidad xm'>{item.cantidad}</div>
       <div className='tableRow__unidades xm'>{item.unidades}</div>
-      <div className='tableRow__precio xm'>$ {item.precioUnit}</div>
+      <div className='tableRow__precio xm'>$ {decimal(item.precioUnit)}</div>
       <div className='tableRow__total xm'>
         <div>{'$'}</div>
         <div>{item.total}</div>
       </div>
       <div className='tableRow__acciones x-s'>
-        <img className='tableRow__acciones__img' src={UpdateIcon} alt='update'/>
+        <img onClick={updateConcepto(index)} className='tableRow__acciones__img' src={UpdateIcon} alt='update'/>
         <img onClick={deleteConcepto(index)} className='tableRow__acciones__img' src={DeleteIcon} alt='del'/>
       </div>
     </div>
@@ -23,7 +28,8 @@ const TableRow = ({item, deleteConcepto, index}) => (
 TableRow.propTypes = {
   item: React.PropTypes.object.isRequired,
   index: React.PropTypes.number.isRequired,
-  deleteConcepto: React.PropTypes.func.isRequired
+  deleteConcepto: React.PropTypes.func.isRequired,
+  updateConcepto: React.PropTypes.func.isRequired
 }
 
 export default TableRow
