aa1fe8ba68672ed70b5a14070eebfb1f350ce4d7
oscaroceguera <oscaroceguera@gmail.com>
2017-04-17 01:10:09 -0500

    [Component] ::: Set current "concepto"

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -14,7 +14,7 @@ const modalStyles = {
   }
 }
 
-const Modal = ({modalOpen, closeModal, handleChange, addConcepto}) => (
+const Modal = ({modalOpen, closeModal, handleChange, addConcepto, currentConcepto}) => (
   <ReactModal
     style={modalStyles}
     isOpen={modalOpen}
@@ -22,10 +22,10 @@ const Modal = ({modalOpen, closeModal, handleChange, addConcepto}) => (
     contentLabel='Agregar concepto'>
     <h1 className='modal__title'>{'Agregar concepto'}</h1>
     <div className='modal__form'>
-      <input name='descripcion' onChange={handleChange} className='modal__form--input' placeholder='Descripción' />
-      <input name='cantidad' onChange={handleChange} className='modal__form--input' placeholder='Cantidad' />
-      <input name='unidades' onChange={handleChange} className='modal__form--input' placeholder='Unidades' />
-      <input name='precioUnit' onChange={handleChange} className='modal__form--input' placeholder='Precio unitario' />
+      <input value={currentConcepto.descripcion} name='descripcion' onChange={handleChange} className='modal__form--input' placeholder='Descripción' />
+      <input value={currentConcepto.cantidad} name='cantidad' onChange={handleChange} className='modal__form--input' placeholder='Cantidad' />
+      <input value={currentConcepto.unidades} name='unidades' onChange={handleChange} className='modal__form--input' placeholder='Unidades' />
+      <input value={currentConcepto.precioUnit} name='precioUnit' onChange={handleChange} className='modal__form--input' placeholder='Precio unitario' />
     </div>
     <div className='modal__agregar'>
       <button onClick={addConcepto} className='modal__agregar--btn'>Agregar</button>
@@ -37,7 +37,8 @@ Modal.propTypes = {
   modalOpen: React.PropTypes.bool.isRequired,
   closeModal: React.PropTypes.func.isRequired,
   handleChange: React.PropTypes.func.isRequired,
-  addConcepto: React.PropTypes.func.isRequired
+  addConcepto: React.PropTypes.func.isRequired,
+  currentConcepto: React.PropTypes.object.isRequired
 }
 
 export default Modal
